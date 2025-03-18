13e32c221485949b306efc6653ec5704d1e47e46
freelife1191 <freelife1191.good@gmail.com>
2025-03-18 22:45:00 +0900

    6번 문제 투포인트 수들의 합 5 다른 풀이

--- src/book/sec3_자료구조/_006_수들의합5/Main.java
+++ src/book/sec3_자료구조/_006_수들의합5/Main.java
@@ -22,22 +22,45 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count = 1;
-        int start_index = 1;
-        int end_index = 1;
-        int sum = 1;
-        while (end_index!=n) {
-            if(sum==n) { // 합의 값이 나왔으므로 증가하고 end_index 를 증가시키고 sum 값에 더해줌
-                count++; end_index++; sum = sum + end_index;
-            } else if(sum>n) { // sum이 n보다 크면 start_index 을 이동시키고 sum 값에서 이동된 start_index 값을 빼줘야 한다
-                sum = sum - start_index;
-                start_index++;
-            } else { // sum이 n보다 작으면 end_index 를 이동시키고 sum 값에 end_index 값을 더한다
-                end_index++; sum = sum + end_index;
+        int n = 15; // 데이터의 개수
+        int m = 15; // 찾고자 하는 부분합
+        int count = 0;
+        int sum = 0;
+        int end = 1;
+        for (int start = 1; start <= n; start++) {
+            // end를 가능한 만큼 이동시키기
+            System.out.println("sum="+sum+", start="+start+", end="+end);
+            while (sum < m) {
+                sum += end;
+                if (end < n) end++;
             }
+            // 부분합이 m일때 카운트 증가
+            if (sum == m) {
+                count++;
+            }
+            sum -= start;
         }
         System.out.println(count);
     }
 }
+/*
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int count = 1; // 15에서 start와 end가 만나서 count 가 1증가되는 상태를 미리 적용
+    int start_index = 1;
+    int end_index = 1;
+    int sum = 1;
+    while (end_index!=n) {
+        if(sum==n) { // 합의 값이 나왔으므로 증가하고 end_index 를 증가시키고 sum 값에 더해줌
+            count++; end_index++; sum = sum + end_index;
+        } else if(sum>n) { // sum이 n보다 크면 start_index 을 이동시키고 sum 값에서 이동된 start_index 값을 빼줘야 한다
+            sum = sum - start_index;
+            start_index++;
+        } else { // sum이 n보다 작으면 end_index 를 이동시키고 sum 값에 end_index 값을 더한다
+            end_index++; sum = sum + end_index;
+        }
+    }
+    System.out.println(count);
+}
+*/
